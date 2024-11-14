99a6de4f60d1a82546cc6918ccfaacd570b6f773
jz1248 <jz1248it@gmail.com>
2024-11-14 22:00:11 +0800

    Update 20241114

--- src/priv/zzy/solutions/T01547.java
+++ src/priv/zzy/solutions/T01547.java
@@ -2,10 +2,30 @@ package priv.zzy.solutions;
 
 import priv.zzy.Solution;
 
+import java.util.Arrays;
+
 public class T01547 extends Solution {
 
     public int minCost(int n, int[] cuts) {
-        int[][] dp = new int[n + 1][cuts.length + 1];
+        int m = cuts.length;
+        Arrays.sort(cuts);
+        int[] cutsNew = new int[m + 2];
+        System.arraycopy(cuts, 0, cutsNew, 1, m);
+        cutsNew[0] = 0;
+        cutsNew[m + 1] = n;
+
+        int[][] dp = new int[m + 2][m + 2];
+
+        for (int i = m; i >= 1; i--) {
+            for (int j = i; j <= m; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k - 1] + dp[k + 1][j] + (cutsNew[j + 1] - cutsNew[i - 1]));
+                }
+            }
+        }
+
+        return dp[1][m];
     }
 
     @Override
